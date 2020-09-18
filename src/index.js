5fb73bfbc4279ec1dbee596f2aeefa9df1c7f3fd
guanshuaijie <1845543324@qq.com>
2020-09-18 19:55:16 +0800

    opt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const hash = location.hash.slice(1).split('/')
-const filepath = hash.reduce((pre, cur) => {
+const hash = location.hash && location.hash.slice(1).split('/')
+const filepath = hash ? hash.reduce((pre, cur) => {
   return `${pre}/${cur}`
-})
+}) : 'es-2021/index'
 import(`./${filepath}.js`)
+
+// console.log(require.context('./', true, /\.js/).keys())
